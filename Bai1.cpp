b0c5651553b858f6715b7250b5ccb620d0bd9168
76DCHT22189-16-Nguyentiendung <dnt290407@gmail.com>
2026-08-28 10:09:56 +0700

    Add MaTran class for matrix representation
    Define a class for matrix representation with member variables for rows, columns, and a 2D array.

---
+++ Bai1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>     // Thư viện dùng cho các lệnh nhập xuất
+using namespace std;    // Giúp sử dụng cin, cout
+
+// ================= KHAI BÁO LỚP MA TRẬN =================
+class MaTran {
+private:
+
+    // Biến lưu số hàng của ma trận
+    int soHang;
+
+    // Biến lưu số cột của ma trận
+    int soCot;
+
+    // Mảng hai chiều dùng để lưu các phần tử của ma trận
+    int a[100][100];
+
+};
+
+
+// ================= HÀM MAIN =================
+int main() {
+
+    // Chương trình kết thúc
+    return 0;
+}
